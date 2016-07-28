96342b1e25b0a660f240d899fda1b2db4bf9c98a
erichkko <erichkko@163.com>
2016-07-28 23:19:12 +0800

    init pro
    初始化app

---
+++ NewsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsDemo
+//
+//  Created by wanglong on 16/7/28.
+//  Copyright © 2016年 wanglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
