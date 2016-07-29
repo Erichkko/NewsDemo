3877d83d6dbb6b493a0fb7ffdcff404d51d178c3
erichkko <erichkko@163.com>
2016-07-29 23:46:25 +0800

    done
    应用间的跳转

--- NewsDemo/ViewController.m
+++ NewsDemo/ViewController.m
@@ -23,5 +23,31 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)jump2weChat:(id)sender {
+    
+    NSURL *wechatUrl = [NSURL URLWithString:@"wechat://?news"];
+    [self openUrl:wechatUrl];
+   
+}
+- (IBAction)jump2Moments:(id)sender {
+    
+    NSURL *wechatMomentsUrl = [NSURL URLWithString:@"wechat://moments?news"];
+    [self openUrl:wechatMomentsUrl];
+    
+}
+- (IBAction)jump2Contacts:(id)sender {
+    
+    NSURL *wechatContactsUrl = [NSURL URLWithString:@"wechat://contacts?news"];
+    [self openUrl:wechatContactsUrl];
+}
 
+- (void)openUrl:(NSURL *)url
+{
+    
+    UIApplication *app = [UIApplication sharedApplication] ;
+    if ([app canOpenURL:url]) {
+        [app openURL:url];
+    }
+    
+}
 @end
